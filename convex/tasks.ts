474f1bb7e47c61b5487faea071e4ba6df7877542
florian.bar <florian.bar@muvraline.fr>
2024-11-07 15:59:59 +0100

    Fix

--- convex/tasks.ts
+++ convex/tasks.ts
@@ -1,8 +1,36 @@
 import { query } from "./_generated/server";
+import { mutation } from "./_generated/server";
+import { v } from "convex/values";
 
 export const get = query({
   args: {},
   handler: async (ctx) => {
     return await ctx.db.query("tasks").collect();
+  },
+});
+
+export const createTask = mutation({
+  args: { text: v.string(), isCompleted: v.boolean() },
+  handler: async (ctx, args) => {
+    const taskId = { text: args.text, isCompleted: args.isCompleted };
+    return await ctx.db.insert("tasks", taskId);
+  },
+});
+
+export const deleteTask = mutation({
+  args: { id: v.id("tasks") },
+  handler: async (ctx, args) => {
+    await ctx.db.delete(args.id);
+  },
+});
+
+export const updateTask = mutation({
+  args: {
+    id: v.id("tasks"),         
+    text: v.string(),      
+    isCompleted: v.boolean(),
+  },
+  handler: async (ctx, { id, text, isCompleted }) => {
+    await ctx.db.patch(id, { text, isCompleted });
   },
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "../convex/_generated/api";
-import { Authenticated, Unauthenticated, AuthLoading } from "convex/react";
+import { Authenticated, Unauthenticated } from "convex/react";
 import { SignIn } from "./SignIn";
 import { SignOut } from "./SignOut"
+import { useState } from "react";
+import { Id } from "../convex/_generated/dataModel";
 
 function App() {
   return (
@@ -13,7 +15,12 @@ function App() {
       </Unauthenticated>
       <Authenticated>  
         <SignOut />
+        <br></br>
+        <br></br>
         <Content />
+        <br></br>
+        <Create />
+        <br></br>
       </Authenticated>
     </>
   );
@@ -21,10 +28,62 @@ function App() {
 
 function Content() {
   const tasks = useQuery(api.tasks.get);
+  const remove = useMutation(api.tasks.deleteTask);
+  const updateTask = useMutation(api.tasks.updateTask);
+
+  const handleUpdateTask = async (id: Id<"tasks">, text: string, isCompleted: boolean) => {
+    await updateTask({ id, text, isCompleted: !isCompleted });
+  };
+
+  const handleDeleteTask = async (id: Id<"tasks">) => {
+    await remove({ id });
+  };
   return (
     <div className="App">
-      {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+      <ul>
+        {tasks?.map(({ _id, text, isCompleted }) => 
+        <li>            
+            <span
+              style={{
+                textDecoration: isCompleted ? "line-through" : "none",
+                cursor: "pointer",
+              }}
+              onClick={() => handleUpdateTask(_id, text, isCompleted)}
+              key={_id}
+            >
+              {text}
+            </span>
+            <button onClick={() => handleDeleteTask(_id)} style={{ marginLeft: "10px" }}>
+              Delete
+            </button>
+        </li>)}
+      </ul>
     </div>
+  );
+}
+
+function Create(){
+  const [newTaskText, setNewTaskText] = useState<string>("");
+  const [newIsCompleted, setNewIsCompleted] = useState<boolean>(false);
+  const create = useMutation(api.tasks.createTask);
+
+  const handleCreateTask = async () => {
+    if (newTaskText.trim()) {
+      await create({ text: newTaskText, isCompleted: newIsCompleted });
+      setNewIsCompleted(false);
+    }
+  };
+
+  return(
+    <div>
+        <input
+          type="text"
+          value={newTaskText}
+          onChange={(e) => setNewTaskText(e.target.value)}
+          placeholder="Add a new task"
+        />
+        <button onClick={handleCreateTask}>Add Task</button>
+      </div>
   );
 }
 
